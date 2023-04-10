0d9b0a2c2b17582f83187408a48eae5ec7ed090e
Stephen <stephen@mapsmith.net>
2023-04-10 10:24:57 -0400

    file size #8

--- separate_rasters.py
+++ separate_rasters.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from osgeo import gdal
 
-def separate_rasters_by_color(input_file, output_folder, color_values):
+def separate_rasters_by_color(input_file, output_folder, color_values, resampling_factor=1):
     print("Separating rasters by color...")
 
     # Open the input raster file
@@ -10,17 +10,21 @@ def separate_rasters_by_color(input_file, output_folder, color_values):
     src_band = src_ds.GetRasterBand(1)
     x_size, y_size = src_ds.RasterXSize, src_ds.RasterYSize
 
+    # Define resampled dimensions
+    x_resampled_size = int(x_size / resampling_factor)
+    y_resampled_size = int(y_size / resampling_factor)
+
     # Define block size and number of blocks
     block_size = 1024
-    n_blocks_x = int(np.ceil(x_size / block_size))
-    n_blocks_y = int(np.ceil(y_size / block_size))
+    n_blocks_x = int(np.ceil(x_resampled_size / block_size))
+    n_blocks_y = int(np.ceil(y_resampled_size / block_size))
 
     # Create output rasters
     output_rasters = {}
     for color_value in color_values:
         output_file = os.path.join(output_folder, f"{color_value}_global.tif")
         driver = gdal.GetDriverByName("GTiff")
-        dst_ds = driver.Create(output_file, x_size, y_size, 1, gdal.GDT_Byte)
+        dst_ds = driver.Create(output_file, x_resampled_size, y_resampled_size, 1, gdal.GDT_Byte, options=['COMPRESS=LZW'])
         dst_ds.SetGeoTransform(src_ds.GetGeoTransform())
         dst_ds.SetProjection(src_ds.GetProjection())
         output_rasters[color_value] = dst_ds.GetRasterBand(1)
@@ -28,17 +32,20 @@ def separate_rasters_by_color(input_file, output_folder, color_values):
     # Loop through the raster in blocks
     for i in range(n_blocks_x):
         for j in range(n_blocks_y):
-            x_start = i * block_size
-            y_start = j * block_size
+            x_start = i * block_size * resampling_factor
+            y_start = j * block_size * resampling_factor
 
-            x_block_size = min(block_size, x_size - x_start)
-            y_block_size = min(block_size, y_size - y_start)
+            x_block_size = min(block_size * resampling_factor, x_size - x_start)
+            y_block_size = min(block_size * resampling_factor, y_size - y_start)
 
             src_array = src_band.ReadAsArray(x_start, y_start, x_block_size, y_block_size)
 
+            if resampling_factor > 1:
+                src_array = src_array[::resampling_factor, ::resampling_factor]
+
             for color_value in color_values:
                 mask = np.where(src_array == color_value, 1, 0).astype(np.uint8)
-                output_rasters[color_value].WriteArray(mask, x_start, y_start)
+                output_rasters[color_value].WriteArray(mask, x_start // resampling_factor, y_start // resampling_factor)
 
     # Clean up
     for color_value in output_rasters:
