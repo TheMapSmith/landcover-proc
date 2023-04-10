994eef03f4d06328e6e07c896a14087f20ccf9e9
Stephen <stephen@mapsmith.net>
2023-04-10 10:13:56 -0400

    #7

--- separate_rasters.py
+++ separate_rasters.py
@@ -15,10 +15,15 @@ def separate_rasters_by_color(input_file, output_folder, color_values):
     n_blocks_x = int(np.ceil(x_size / block_size))
     n_blocks_y = int(np.ceil(y_size / block_size))
 
-    # Create an empty array for each output raster
+    # Create output rasters
     output_rasters = {}
     for color_value in color_values:
-        output_rasters[color_value] = np.zeros((y_size, x_size), dtype=np.uint8)
+        output_file = os.path.join(output_folder, f"{color_value}_global.tif")
+        driver = gdal.GetDriverByName("GTiff")
+        dst_ds = driver.Create(output_file, x_size, y_size, 1, gdal.GDT_Byte)
+        dst_ds.SetGeoTransform(src_ds.GetGeoTransform())
+        dst_ds.SetProjection(src_ds.GetProjection())
+        output_rasters[color_value] = dst_ds.GetRasterBand(1)
 
     # Loop through the raster in blocks
     for i in range(n_blocks_x):
@@ -33,25 +38,11 @@ def separate_rasters_by_color(input_file, output_folder, color_values):
 
             for color_value in color_values:
                 mask = np.where(src_array == color_value, 1, 0).astype(np.uint8)
-                output_rasters[color_value][y_start:y_start+y_block_size, x_start:x_start+x_block_size] = mask
-
-    # Save the output rasters
-    for color_value in color_values:
-        print(f"Processing color value: {color_value}")
-
-        output_file = os.path.join(output_folder, f"{color_value}_global.tif")
-        driver = gdal.GetDriverByName("GTiff")
-        dst_ds = driver.Create(output_file, x_size, y_size, 1, gdal.GDT_Byte)
-
-        dst_ds.SetGeoTransform(src_ds.GetGeoTransform())
-        dst_ds.SetProjection(src_ds.GetProjection())
-
-        dst_band = dst_ds.GetRasterBand(1)
-        dst_band.WriteArray(output_rasters[color_value])
-
-        # Clean up
-        dst_band, dst_ds = None, None
+                output_rasters[color_value].WriteArray(mask, x_start, y_start)
 
     # Clean up
+    for color_value in output_rasters:
+        output_rasters[color_value] = None
+
     src_band, src_ds = None, None
     print("Separation of rasters by color complete.")
