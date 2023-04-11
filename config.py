432c1742c96857ba24aeb7412d391bd799e81c16
Stephen <stephen@mapsmith.net>
2023-04-10 22:49:16 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 input_folder = "input_folder"
 output_folder = "output_folder"
 color_values = [
-    0, 111, 113, 112, 114, 115, 116, 121, 123, 122, 124, 125, 126,
+    111, 113, 112, 114, 115, 116, 121, 123, 122, 124, 125, 126,
     20, 30, 90, 100, 60, 40, 50, 70, 80, 200
-]
+] # removed 0 - missing data
