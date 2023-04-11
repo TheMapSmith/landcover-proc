535fb7e23e5d08ca19161a3bb2efe4d575e6e497
Stephen <stephen@mapsmith.net>
2023-04-11 17:37:23 -0400

    #11

---
+++ compress_ratser.py
@@ -0,0 +1,23 @@
+import os
+from osgeo import gdal
+
+def compress_raster(input_file, output_file):
+    print(f"Compressing {input_file} to {output_file} using LZW compression...")
+
+    # Open input raster
+    src_ds = gdal.Open(input_file)
+
+    # Compress raster using LZW compression
+    compressed_ds = gdal.Translate(output_file, src_ds,
+                                   options=gdal.TranslateOptions(creationOptions=["COMPRESS=LZW"]))
+
+    # Close datasets
+    compressed_ds = None
+    src_ds = None
+
+    print("Compression complete.")
+
+input_file = "source_folder/landcover_resampled_30km.tif"
+output_file = "input_folder/landcover_compressed_30km.tif"
+
+compress_raster(input_file, output_file)
