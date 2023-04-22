4714d3acf1dcc6585b2b8b2366334ffb3ef89154
Stephen <stephen@mapsmith.net>
2023-04-22 15:55:47 -0400

    Update process_folder.py

--- process_folder.py
+++ process_folder.py
@@ -32,7 +32,7 @@ def process_input_folder(input_folder, output_folder, color_values):
     # merge the forest rasters 
     forest_values = [111, 113, 112, 114, 115, 116, 121, 122, 124, 125, 126]
     input_files = [os.path.join(output_folder, f"{color}_blurred.tif") for color in color_values]
-    merged_blurred_output = os.path.join(output_folder, "forest_blurred.tif")
+    merged_blurred_output = "forest_blurred.tif"
     merge_rasters(input_files, output_folder, merged_blurred_output)
 
 
