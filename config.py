f40d5ab20f5c16d61fe3662957dd6df00b8a5887
Stephen <stephen@mapsmith.net>
2023-04-23 14:32:00 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
+# Configuration
+landcover_color_values = [20, 30, 90, 100, 60, 40, 50, 70, 80, 200]
+forest_color_values = [111, 113, 112, 114, 115, 116, 121, 122, 123, 124, 125, 126]
+all_color_values = [111, 113, 112, 114, 115, 116, 121, 122, 123, 124, 125, 126, 20, 30, 90, 100, 60, 40, 50, 70, 80, 200]
+processing_color_values = [20, 30, 90, 100, 60, 40, 50, 70, 80, 200, "forest"]
+
 input_folder = "input_folder"
 output_folder = "output_folder"
-color_values = [
-    111, 113, 112, 114, 115, 116, 121, 122, 124, 125, 126,
-    20, 30, 90, 100, 60, 40, 50, 70, 80, 200
-] # removed 0 - missing data
-
-forest_values = [111, 113, 112, 114, 115, 116, 121, 122, 124, 125, 126]
