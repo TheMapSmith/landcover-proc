20ce68ffe159d9f94c6730e045640bf755638b80
Stephen <stephen@mapsmith.net>
2023-04-23 14:39:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if not os.path.exists(output_folder):
 def main():
     # Step 1: Separate global landcover raster into separate rasters by class
     # Step 2: Simplify and generalize each class raster
-    process_input_folder(input_folder, output_folder, color_values)
+    process_input_folder(input_folder, output_folder, all_color_values, forest_color_values)
 
 if __name__ == "__main__":
     # Step 0: Start a multi-threaded parallel process
